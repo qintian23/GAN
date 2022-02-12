245d69af9356cd20c0d50bfb77bb85b0409a27c2
Liucheng Zhu <13927181971@163.com>
2022-02-12 20:02:57 +0800

    The 3st commit

---
+++ gan.py
@@ -0,0 +1,97 @@
+# 模型, 优化器，求导函数
+import torch
+from torch import nn,optim,autograd
+import numpy as np
+import visdom
+import random
+
+h_dim=400
+batchsz=512
+viz=visdom.Visdom()
+
+class Generator(nn.Module):
+
+    def __init__(self):
+        super(Generator, self).__init__()
+
+        self.net=nn.Sequential(
+            # z: [b,2] => [b,2]
+            nn.Linear(2,h_dim),
+            nn.ReLU(True),
+            nn.Linear(h_dim, h_dim),
+            nn.ReLU(True),
+            nn.Linear(h_dim, h_dim),
+            nn.ReLU(True),
+            nn.Linear(h_dim, 2)
+        )
+
+    def forward(self,z):
+        output=self.net(z)
+        return output
+
+class Discriminator(nn.Module):
+
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        self.net = nn.Sequential(
+            # z: [b,2] => [b,2]
+            nn.Linear(2, h_dim),
+            nn.ReLU(True),
+            nn.Linear(h_dim, h_dim),
+            nn.ReLU(True),
+            nn.Linear(h_dim, h_dim),
+            nn.ReLU(True),
+            nn.Linear(h_dim, 1),
+            nn.Sigmoid()
+        )
+
+        def forward(self, x):
+            output = self.net(x)
+            return output.view(-1)
+
+def data_generator():
+    """
+    8-gaussian mixture models
+    :return:
+    """
+    scale=2.
+    centers=[
+        (1,0),
+        (-1,0),
+        (0,1),
+        (0,-1),
+        (1. / np.sqrt(2), 1. / np.sqrt(2)),
+        (1. / np.sqrt(2), -1. / np.sqrt(2)),
+        (-1. / np.sqrt(2), 1. / np.sqrt(2)),
+        (-1. / np.sqrt(2), -1. / np.sqrt(2))
+    ]
+    centers=[(scale*x,scale*y) for x,y in centers]
+
+    while True:
+        dataset=[]
+
+        for i in range(batchsz):
+
+            point=np.random.randn(2)*0.02
+            center=random.choice(centers)
+            # N(0,1) + center_x1/x2
+            point[0] += center[0]
+            point[1] += center[1]
+            dataset.append(point)
+
+        dataset=np.array(dataset).astype(np.float32)
+        dataset /= 1.414
+        yield dataset
+
+def main():
+
+    data_iter=data_generator()
+    x=next(data_iter)
+    # [b,2]
+    # print(x.shape)
+
+
+if __name__ == '__main__':
+    main()
+
